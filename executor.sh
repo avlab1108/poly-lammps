b164e1a6cefb78994056adeb90b03c2166bc98c9
_ <catena@protonmail.com>
2016-01-18 09:53:38 +0300

    check that folder exists

--- executor.sh
+++ executor.sh
@@ -9,13 +9,15 @@ do
   do
     FOLD="run-$(($i*4 + $j))"
     echo "folder = $FOLD"
-    mkdir $FOLD
-    sed "s/\<randomSeed\>/$RANDOM/g" $IN_FILE > in.txt
-    cp in.txt $FOLD 
-    cp $CHAIN_FILE $FOLD 
-    cd $FOLD
-    lmp_mpi < in.txt > execute.log 2>&1 &   
-    cd ..
+    if [ ! -d "$FOLD" ]; then
+      mkdir $FOLD
+      sed "s/\<randomSeed\>/$RANDOM/g" $IN_FILE > in.txt
+      cp in.txt $FOLD 
+      cp $CHAIN_FILE $FOLD 
+      cd $FOLD
+      lmp_mpi < in.txt > execute.log 2>&1 &   
+      cd ..
+    fi
   done
   wait
 done
