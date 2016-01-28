5f3987665aacde657817a0886edf5d41d27422c9
_ <canoneer@mail.ru>
2016-01-28 19:43:07 +0300

    512 torus

--- elastic_init/elastic.sh
+++ elastic_init/elastic.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 CONFIG="/home/dmitry/Code/poly-lammps/elastic_init"
-BASE="/run/user/1000/gvfs/smb-share:server=nanomachines,share=modeling/linear/128/globule/LAMMPS"
+BASE="/home/dmitry/NAS/linear/128/globule/LAMMPS"
 
-for i in {0..100}
+for i in {1..100}
 do
     FOLD="$BASE/run-$i"
     echo "folder = $FOLD"
     if [ -d "$FOLD" ]; then
       cd $FOLD
       cp $CONFIG/* .
+      ./run.sh -e
       ./run.sh -n 1 -t
+      sleep 10
     fi
 done
